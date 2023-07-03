4ae5a56d27d20e9b2e0750ae486cd244cdbb4bc9
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-07-03 16:57:32 +0200

    Eliminate duplicate code

--- tests/src/DatabaseTestCase.php
+++ tests/src/DatabaseTestCase.php
@@ -1,32 +1,19 @@
 <?php declare(strict_types=1);
 namespace example\framework\database;
 
+use example\framework\Factory;
 use PHPUnit\Framework\TestCase;
 
 abstract class DatabaseTestCase extends TestCase
 {
     protected function connectionForReadingEvents(): MysqlDatabase
     {
-        return MysqlDatabase::connect(
-            new MysqlDatabaseConfiguration(
-                'localhost',
-                'event_reader',
-                'event_reader_password',
-                'caledonia',
-            ),
-        );
+        return (new Factory)->eventReaderDatabase();
     }
 
     protected function connectionForWritingEvents(): MysqlDatabase
     {
-        return MysqlDatabase::connect(
-            new MysqlDatabaseConfiguration(
-                'localhost',
-                'event_writer',
-                'event_writer_password',
-                'caledonia',
-            ),
-        );
+        return (new Factory)->eventWriterDatabase();
     }
 
     protected function emptyTable(string $table): void
