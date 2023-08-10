fd588aaecfefb0702caa5ca53dfee469cfb80d16
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-10 15:23:32 +0200

    Refactor

--- src/domain/market/Market.php
+++ src/domain/market/Market.php
@@ -1,43 +1,18 @@
 <?php declare(strict_types=1);
 namespace example\caledonia\domain;
 
-use function max;
-use function min;
+use function range;
 
 final class Market
 {
-    private readonly PriceTable $prices;
+    /**
+     * @psalm-var array<'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', PriceTable>
+     */
+    private array $priceTables;
 
     /**
-     * @psalm-var int<0, 9>
+     * @throws PriceMustBePositiveException
      */
-    private int $bread;
-
-    /**
-     * @psalm-var int<0, 9>
-     */
-    private int $cheese;
-
-    /**
-     * @psalm-var int<0, 9>
-     */
-    private int $grain;
-
-    /**
-     * @psalm-var int<0, 9>
-     */
-    private int $milk;
-
-    /**
-     * @psalm-var int<0, 9>
-     */
-    private int $whisky;
-
-    /**
-     * @psalm-var int<0, 9>
-     */
-    private int $wool;
-
     public static function create(): self
     {
         return self::from(
@@ -57,11 +32,12 @@ final class Market
      * @psalm-param int<0, 9> $milk
      * @psalm-param int<0, 9> $whisky
      * @psalm-param int<0, 9> $wool
+     *
+     * @throws PriceMustBePositiveException
      */
     public static function from(int $bread, int $cheese, int $grain, int $milk, int $whisky, int $wool): self
     {
         return new self(
-            new PriceTable,
             $bread,
             $cheese,
             $grain,
@@ -78,103 +54,43 @@ final class Market
      * @psalm-param int<0, 9> $milk
      * @psalm-param int<0, 9> $whisky
      * @psalm-param int<0, 9> $wool
+     *
+     * @throws PriceMustBePositiveException
      */
-    private function __construct(PriceTable $prices, int $bread, int $cheese, int $grain, int $milk, int $whisky, int $wool)
+    private function __construct(int $bread, int $cheese, int $grain, int $milk, int $whisky, int $wool)
     {
-        $this->prices = $prices;
-        $this->bread  = $bread;
-        $this->cheese = $cheese;
-        $this->grain  = $grain;
-        $this->milk   = $milk;
-        $this->whisky = $whisky;
-        $this->wool   = $wool;
+        $this->priceTables = [
+            'bread'  => PriceTable::bread($bread),
+            'cheese' => PriceTable::cheese($cheese),
+            'grain'  => PriceTable::grain($grain),
+            'milk'   => PriceTable::milk($milk),
+            'whisky' => PriceTable::whisky($whisky),
+            'wool'   => PriceTable::wool($wool),
+        ];
     }
 
     public function priceFor(Good $good): Price
     {
-        $prices = $this->prices->for($good);
-
-        return match ($good::class) {
-            Bread::class  => $prices[$this->bread],
-            Cheese::class => $prices[$this->cheese],
-            Grain::class  => $prices[$this->grain],
-            Milk::class   => $prices[$this->milk],
-            Whisky::class => $prices[$this->whisky],
-            Wool::class   => $prices[$this->wool],
-        };
+        return $this->priceTables[$good->asString()]->current();
     }
 
+    /**
+     * @psalm-param positive-int $amount
+     */
     public function purchase(Good $good, int $amount): void
     {
-        if ($good->isBread()) {
-            $this->bread = min(9, $this->bread + $amount);
-
-            return;
-        }
-
-        if ($good->isCheese()) {
-            $this->cheese = min(9, $this->cheese + $amount);
-
-            return;
-        }
-
-        if ($good->isGrain()) {
-            $this->grain = min(9, $this->grain + $amount);
-
-            return;
-        }
-
-        if ($good->isMilk()) {
-            $this->milk = min(9, $this->milk + $amount);
-
-            return;
-        }
-
-        if ($good->isWhisky()) {
-            $this->whisky = min(9, $this->whisky + $amount);
-
-            return;
-        }
-
-        if ($good->isWool()) {
-            $this->wool = min(9, $this->wool + $amount);
+        foreach (range(1, $amount) as $i) {
+            $this->priceTables[$good->asString()]->increase();
         }
     }
 
+    /**
+     * @psalm-param positive-int $amount
+     */
     public function sell(Good $good, int $amount): void
     {
-        if ($good->isBread()) {
-            $this->bread = max(0, $this->bread - $amount);
-
-            return;
-        }
-
-        if ($good->isCheese()) {
-            $this->cheese = max(0, $this->cheese - $amount);
-
-            return;
-        }
-
-        if ($good->isGrain()) {
-            $this->grain = max(0, $this->grain - $amount);
-
-            return;
-        }
-
-        if ($good->isMilk()) {
-            $this->milk = max(0, $this->milk - $amount);
-
-            return;
-        }
-
-        if ($good->isWhisky()) {
-            $this->whisky = max(0, $this->whisky - $amount);
-
-            return;
-        }
-
-        if ($good->isWool()) {
-            $this->wool = max(0, $this->wool - $amount);
+        foreach (range(1, $amount) as $i) {
+            $this->priceTables[$good->asString()]->decrease();
         }
     }
 }
--- src/domain/market/PriceTable.php
+++ src/domain/market/PriceTable.php
@@ -1,20 +1,188 @@
 <?php declare(strict_types=1);
 namespace example\caledonia\domain;
 
-final readonly class PriceTable
+final class PriceTable
 {
     /**
-     * @psalm-return non-empty-list<Price>
+     * @psalm-var list<Price>
      */
-    public function for(Good $good): array
+    private readonly array $prices;
+
+    /**
+     * @psalm-var int<0, 9>
+     */
+    private int $position;
+
+    /**
+     * @psalm-param int<0, 9> $position
+     *
+     * @throws PriceMustBePositiveException
+     */
+    public static function bread(int $position): self
     {
-        return match ($good::class) {
-            Bread::class  => [Price::from(7), Price::from(8), Price::from(9), Price::from(10), Price::from(11), Price::from(11), Price::from(12), Price::from(13), Price::from(14), Price::from(15)],
-            Cheese::class => [Price::from(7), Price::from(8), Price::from(9), Price::from(10), Price::from(11), Price::from(12), Price::from(13), Price::from(14), Price::from(14), Price::from(15)],
-            Grain::class  => [Price::from(3), Price::from(3), Price::from(4), Price::from(5), Price::from(6), Price::from(6), Price::from(7), Price::from(7), Price::from(8), Price::from(8)],
-            Milk::class   => [Price::from(3), Price::from(4), Price::from(5), Price::from(5), Price::from(6), Price::from(6), Price::from(7), Price::from(7), Price::from(8), Price::from(8)],
-            Whisky::class => [Price::from(8), Price::from(9), Price::from(10), Price::from(11), Price::from(12), Price::from(13), Price::from(13), Price::from(14), Price::from(15), Price::from(16)],
-            Wool::class   => [Price::from(3), Price::from(3), Price::from(4), Price::from(4), Price::from(5), Price::from(5), Price::from(6), Price::from(6), Price::from(7), Price::from(8)],
-        };
+        return new self(
+            [
+                Price::from(7),
+                Price::from(8),
+                Price::from(9),
+                Price::from(10),
+                Price::from(11),
+                Price::from(11),
+                Price::from(12),
+                Price::from(13),
+                Price::from(14),
+                Price::from(15),
+            ],
+            $position,
+        );
+    }
+
+    /**
+     * @psalm-param int<0, 9> $position
+     *
+     * @throws PriceMustBePositiveException
+     */
+    public static function cheese(int $position): self
+    {
+        return new self(
+            [
+                Price::from(7),
+                Price::from(8),
+                Price::from(9),
+                Price::from(10),
+                Price::from(11),
+                Price::from(12),
+                Price::from(13),
+                Price::from(14),
+                Price::from(14),
+                Price::from(15),
+            ],
+            $position,
+        );
+    }
+
+    /**
+     * @psalm-param int<0, 9> $position
+     *
+     * @throws PriceMustBePositiveException
+     */
+    public static function grain(int $position): self
+    {
+        return new self(
+            [
+                Price::from(3),
+                Price::from(3),
+                Price::from(4),
+                Price::from(5),
+                Price::from(6),
+                Price::from(6),
+                Price::from(7),
+                Price::from(7),
+                Price::from(8),
+                Price::from(8),
+            ],
+            $position,
+        );
+    }
+
+    /**
+     * @psalm-param int<0, 9> $position
+     *
+     * @throws PriceMustBePositiveException
+     */
+    public static function milk(int $position): self
+    {
+        return new self(
+            [
+                Price::from(3),
+                Price::from(4),
+                Price::from(5),
+                Price::from(5),
+                Price::from(6),
+                Price::from(6),
+                Price::from(7),
+                Price::from(7),
+                Price::from(8),
+                Price::from(8),
+            ],
+            $position,
+        );
+    }
+
+    /**
+     * @psalm-param int<0, 9> $position
+     *
+     * @throws PriceMustBePositiveException
+     */
+    public static function whisky(int $position): self
+    {
+        return new self(
+            [
+                Price::from(8),
+                Price::from(9),
+                Price::from(10),
+                Price::from(11),
+                Price::from(12),
+                Price::from(13),
+                Price::from(13),
+                Price::from(14),
+                Price::from(15),
+                Price::from(16),
+            ],
+            $position,
+        );
+    }
+
+    /**
+     * @psalm-param int<0, 9> $position
+     *
+     * @throws PriceMustBePositiveException
+     */
+    public static function wool(int $position): self
+    {
+        return new self(
+            [
+                Price::from(3),
+                Price::from(3),
+                Price::from(4),
+                Price::from(4),
+                Price::from(5),
+                Price::from(5),
+                Price::from(6),
+                Price::from(6),
+                Price::from(7),
+                Price::from(8),
+            ],
+            $position,
+        );
+    }
+
+    /**
+     * @psalm-param list<Price> $prices
+     * @psalm-param int<0, 9> $position
+     */
+    private function __construct(array $prices, int $position)
+    {
+        $this->prices   = $prices;
+        $this->position = $position;
+    }
+
+    public function current(): Price
+    {
+        return $this->prices[$this->position];
+    }
+
+    public function increase(): void
+    {
+        if ($this->position < 9) {
+            $this->position++;
+        }
+    }
+
+    public function decrease(): void
+    {
+        if ($this->position > 0) {
+            $this->position--;
+        }
     }
 }
