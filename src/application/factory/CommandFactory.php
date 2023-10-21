363fde6a377c53115190770c5bc92c2c2af63107
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-10-21 16:26:09 +0200

    More work on factory

--- src/application/factory/CommandFactory.php
+++ src/application/factory/CommandFactory.php
@@ -3,7 +3,9 @@ namespace example\caledonia\application;
 
 use example\framework\database\MysqlDatabase;
 use example\framework\database\MysqlDatabaseConfiguration;
+use example\framework\event\DatabaseEventReader;
 use example\framework\event\DatabaseEventWriter;
+use example\framework\event\EventReader;
 use example\framework\event\EventWriter;
 use example\framework\event\SubscribableEventDispatcher;
 use example\framework\event\WritingEventSubscriber;
@@ -12,6 +14,21 @@ use example\framework\library\RandomUuidGenerator;
 final readonly class CommandFactory
 {
     use EventJsonMapper;
+
+    public function createPurchaseGoodCommandProcessor(): PurchaseGoodCommandProcessor
+    {
+        return new PurchaseGoodCommandProcessor(
+            $this->createEventEmitter(),
+            $this->createMarketEventSourcer(),
+        );
+    }
+
+    public function createMarketEventSourcer(): MarketEventSourcer
+    {
+        return new MarketEventSourcer(
+            $this->createEventReader(),
+        );
+    }
 
     public function createEventEmitter(): EventEmitter
     {
@@ -23,11 +40,31 @@ final readonly class CommandFactory
         );
     }
 
+    public function createEventReader(): EventReader
+    {
+        return new DatabaseEventReader(
+            $this->createDatabaseConnectionForReadingEvents(),
+            $this->createEventJsonMapper(),
+        );
+    }
+
     private function createEventWriter(): EventWriter
     {
         return new DatabaseEventWriter(
             $this->createDatabaseConnectionForWritingEvents(),
             $this->createEventJsonMapper(),
+        );
+    }
+
+    private function createDatabaseConnectionForReadingEvents(): MysqlDatabase
+    {
+        return MysqlDatabase::connect(
+            new MysqlDatabaseConfiguration(
+                'localhost',
+                'event_reader',
+                'event_reader_password',
+                'caledonia',
+            ),
         );
     }
 
