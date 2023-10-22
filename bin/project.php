6e7170815012375262a88f498f7bc68d4a9fb65d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-10-22 05:48:29 +0200

    Initial work on script for creating projections

---
+++ bin/project.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+namespace example\caledonia\application;
+
+if (!isset($argv[1])) {
+    fwrite(
+        STDERR,
+        sprintf(
+            'Usage: %s <target>' . PHP_EOL,
+            $argv[0]
+        )
+    );
+
+    exit(1);
+}
+
+require __DIR__ . '/../src/autoload.php';
+
+$sourcer = (new QueryFactory)->createMarketEventSourcer();
+
+file_put_contents(
+    $argv[1],
+    (new MarketHtmlProjector)->project($sourcer->source())
+);
