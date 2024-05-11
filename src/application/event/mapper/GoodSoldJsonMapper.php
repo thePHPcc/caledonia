a398a34ee67ebdf9de1cc42d39e6d8acd386aee6
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-05-11 15:47:58 +0200

    Narrow types

--- src/application/event/mapper/GoodSoldJsonMapper.php
+++ src/application/event/mapper/GoodSoldJsonMapper.php
@@ -12,7 +12,7 @@ use example\framework\library\Uuid;
 final class GoodSoldJsonMapper implements EventArrayMapper
 {
     /**
-     * @param array{event_id: string, good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', price: int, amount: int} $data
+     * @param array{event_id: non-empty-string, good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', price: positive-int, amount: positive-int} $data
      */
     public function fromArray(array $data): GoodSoldEvent
     {
@@ -25,7 +25,7 @@ final class GoodSoldJsonMapper implements EventArrayMapper
     }
 
     /**
-     * @return array{good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', price: int, amount: int}
+     * @return array{good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', price: positive-int, amount: positive-int}
      */
     public function toArray(Event $event): array
     {
--- src/application/event/mapper/PriceChangedJsonMapper.php
+++ src/application/event/mapper/PriceChangedJsonMapper.php
@@ -12,7 +12,7 @@ use example\framework\library\Uuid;
 final class PriceChangedJsonMapper implements EventArrayMapper
 {
     /**
-     * @param array{event_id: string, good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', old_price: int, new_price: int} $data
+     * @param array{event_id: non-empty-string, good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', old_price: positive-int, new_price: positive-int} $data
      */
     public function fromArray(array $data): PriceChangedEvent
     {
@@ -25,7 +25,7 @@ final class PriceChangedJsonMapper implements EventArrayMapper
     }
 
     /**
-     * @return array{good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', old_price: int, new_price: int}
+     * @return array{good: 'bread'|'cheese'|'grain'|'milk'|'whisky'|'wool', old_price: positive-int, new_price: positive-int}
      */
     public function toArray(Event $event): array
     {
