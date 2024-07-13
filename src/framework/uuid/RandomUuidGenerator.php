f0da873e7bc29420e3cf894ecfe304ac9093bf53
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-07-13 16:07:58 +0200

    Use sebastian/uuid

--- src/framework/uuid/RandomUuidGenerator.php
+++ src/framework/uuid/RandomUuidGenerator.php
@@ -1,28 +1,12 @@
 <?php declare(strict_types=1);
 namespace example\framework\library;
 
-use function bin2hex;
-use function hexdec;
-use function random_bytes;
-use function sprintf;
-use function substr;
+use function SebastianBergmann\Uuid\uuid;
 
 final readonly class RandomUuidGenerator implements UuidGenerator
 {
     public function generate(): Uuid
     {
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $bytes = bin2hex(random_bytes(16));
-
-        return Uuid::from(
-            sprintf(
-                '%08s-%04s-4%03s-%04x-%012s',
-                substr($bytes, 0, 8),
-                substr($bytes, 8, 4),
-                substr($bytes, 13, 3),
-                hexdec(substr($bytes, 16, 4)) & 0x3FFF | 0x8000,
-                substr($bytes, 20, 12),
-            ),
-        );
+        return Uuid::from(uuid());
     }
 }
