9b0e7815323ebe8009373f501682c66457db0f9e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-11-02 16:58:09 +0100

    Add test

---
+++ tests/unit/framework/event/writer/WriteEventStatementTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+namespace example\framework\event;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\MysqliWrapper\WritingDatabaseConnection;
+
+#[CoversClass(WriteEventStatement::class)]
+#[Group('framework')]
+#[Group('framework/event')]
+#[Small]
+final class WriteEventStatementTest extends TestCase
+{
+    public function testExecutesStatementToWriteEvent(): void
+    {
+        $connection = $this->createMock(WritingDatabaseConnection::class);
+
+        $connection
+            ->expects($this->once())
+            ->method('execute')
+            ->with(
+                'INSERT INTO event
+                         (event_id, topic, payload)
+                  VALUES (?, ?, ?);',
+                'id',
+                'topic',
+                'payload',
+            );
+
+        $statement = new WriteEventStatement('id', 'topic', 'payload');
+
+        $statement->execute($connection);
+
+    }
+}
